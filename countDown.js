8e014ddf356c79d5279f40f3901ba65dd056f9bb
faridhakanda <faridakanda1729@gmail.com>
2025-03-08 14:17:45 +0600

    watch task

--- countDown.js
+++ countDown.js
@@ -1,6 +1,6 @@
 // hourCount, minCount, SecCount
 
-let deadLine = new Date("Mar 7, 2025 23:55:50").getTime();
+let deadLine = new Date("Mar 10, 2025 23:55:50").getTime();
 let x = setInterval(function() {
     let nowTime = new Date().getTime();
     let currentTime = deadLine - nowTime;
@@ -9,9 +9,9 @@ let x = setInterval(function() {
     let hours = Math.floor((currentTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     let minutes = Math.floor((currentTime % (1000 * 60 * 60)) / (1000 * 60));
     let seconds = Math.floor((currentTime % (1000 * 60)) / 1000);
-    document.getElementById("hourCount").innerHTML = "-" + hours;
-    document.getElementById("minCount").innerHTML = "-" + minutes;
-    document.getElementById("secCount").innerHTML = "-" + seconds;
+    document.getElementById("hourCount").innerHTML =  hours;
+    document.getElementById("minCount").innerHTML =  minutes;
+    document.getElementById("secCount").innerHTML = seconds;
     if (currentTime < 0) {
         clearInterval(x);
         document.getElementById("demo").innerHTML = "Expired!";
